ef9c99cd5a2776f1b758630e73ea910fb78a16bb
kafka <bishalkharel0000@gmail.com>
2023-03-30 20:33:50 +0545

    add: W2D6 contents

--- week_2/demoproject/todoapp/forms.py
+++ week_2/demoproject/todoapp/forms.py
@@ -1,8 +1,28 @@
 from django import forms
 from .models import Todo
- 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
  
 class TodoForm(forms.ModelForm):
     class Meta:
-        model = Todo
-        fields = "__all__"
+        model = Todo 
+        fields = "__all__"  
+
+
+# Create your forms here.
+
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password1']
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
